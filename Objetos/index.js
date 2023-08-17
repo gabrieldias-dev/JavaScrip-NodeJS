9e192e9b8574aa4459b20db5fa40482ceed1f60c
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-08-17 10:59:11 -0300

    Add files via upload

---
+++ Objetos/index.js
@@ -0,0 +1,21 @@
+
+
+// Objeto
+// Uma variavel (pen/caneta), e dentro dessa variavel tem as propriedades dela.
+/*
+Nome do item: caneta
+Nome da cor: vermelho
+Preço do item: 3
+ItemDisponivel: sim/true
+
+*/
+
+let pen = {
+    itemName: 'caneta',
+    nameColor: 'vermelho',
+    itemPreco: 3,
+    ItemDisponivel: true
+
+}
+
+console.log(pen)    
